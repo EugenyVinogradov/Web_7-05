edf797e9defa992e0ae9877496275b7743807621
Виноградов Евгений <Vinogradov_e_v@rambler.ru>
2022-11-13 12:56:26 +0300

    Task 2

--- features/reservation.feature
+++ features/reservation.feature
@@ -1,8 +1,14 @@
-Feature: Reservation one ticket today
-    Scenario: Should reservation ticket
-        # Given user is on "/navigation" page
-        # When user search by "тестировщик"
-        # Then user sees the course suggested "Тестировщик ПО"
+Feature: Reservation ticket
+    Scenario: Should reservation ticket one ticket today
         Given user is on "http://qamid.tmweb.ru/client/index.php" page
-        When user selects the time of the movie screening "19:00" tomorrow
-        # Then
+        When user books a ticket for the session tomorrow at "19:00", seat "5" in the "2" st row
+        Then user sees the inscription "Вы выбрали билеты:" and sees the reserved seat "5" in the row "2"
+# Feature: Reservation tickets    
+    Scenario: Should reservation two tickets on fourth day
+        Given user is on "http://qamid.tmweb.ru/client/index.php" page
+        When user books two tickets for the session on "4" day at "14:00", seats "4" and "5" in the "3" st row    
+        Then user sees the inscription "Вы выбрали билеты:" and sees the reserved seat "4" and "5" in the row "3"   
+     Scenario: Not reservation one ticket today  to already reserved seat
+        Given user is on "http://qamid.tmweb.ru/client/index.php" page
+        When user books a ticket for the session tomorrow at "19:00", seat "6" in the "10" st row, and this seat is already reserved   
+        Then user sees that the resrvation button is inactive     
--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -4,7 +4,7 @@ const chai = require("chai");
 const expect = chai.expect;
 const { Given, When, Then, Before, After, setDefaultTimeout } = require("cucumber");
 const { clickElement, clickElementXPath, getText } = require("../../lib/commands.js");
-const { reservationOneOrTwoTikets } = require("../../lib/commands2.js");
+const { reservationOneOrTwoTikets, reservationOneOrTwoTiketsXPath } = require("../../lib/commands2.js");
 
 
 Before(async function () {
@@ -19,22 +19,77 @@ After(async function () {
     await this.browser.close();
   }
 });
-Given("user is on {string} page", async function (string) {
-    return await this.page.goto(`${string}`, {
-      setTimeout: 20000,
+Given("user is on {string} page", { timeout: 30000}, async function (string) {
+    return await this.page.goto(`${string}`);
+  });
+  
+  When("user books a ticket for the session tomorrow at {string}, seat {string} in the {string} st row", 
+    { timeout: 15000}, async function (string1, string2, string3) {
+      await clickElement(this.page, "body > nav > a:nth-child(2)");
+      await reservationOneOrTwoTiketsXPath(this.page, `//*[text()='${string1}']`, 
+      `body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(${string3}) > span:nth-child(${string2})`, 
+      "body > main > section > div.buying__info > div > h2",
+      "//*[text()='Забронировать']");
     });
-  });
   
-  When("user selects the time of the movie screening {string} tomorrow" , async function (string) {
-    await clickElement(this.page, "body > nav > a:nth-child(2)");
-    await clickElementXPath(this.page, `'//*[text()='${string}']'`);
-    console.log(`'//*[text()='${string}']'`);
-
-
+  Then("user sees the inscription {string} and sees the reserved seat {string} in the row {string}", async function (string1, string2, string3) {
+    const actual1 = await getText(this.page, ".ticket__check-title");
+    const expected1 = await string1;
+    expect(actual1).contains(expected1);
+    const seats = await getText(this.page, "body > main > section > div > p:nth-child(2) > span");
+    const actual2 = await seats.split("/")[0];
+    const expected2 = await string3;
+    expect(actual2).equal(expected2);
+    const actual3 = await seats.split("/")[1];;
+    const expected3 = await string2;
+    expect(actual3).contains(expected3);
   });
-  
-  Then("user sees the course suggested {string}", async function (string) {
-    const actual = await getText(this.page, "a[data-name]");
-    const expected = await string;
-    expect(actual).contains(expected);
-  });
+
+  When("user books two tickets for the session on {string} day at {string}, seats {string} and {string} in the {string} st row",
+    { timeout: 15000}, async function (string1, string2, string3, string4, string5) {
+      await clickElement(this.page, `body > nav > a:nth-child(${string1})`);
+      await reservationOneOrTwoTiketsXPath(this.page, `//*[text()='${string2}']`, 
+      `body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(${string5}) > span:nth-child(${string3})`, 
+      `body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(${string5}) > span:nth-child(${string4})`,
+      "//*[text()='Забронировать']");
+    });
+
+    Then("user sees the inscription {string} and sees the reserved seat {string} and {string} in the row {string}", async function (string1, string2, string3, string4) {
+      const actual1 = await getText(this.page, ".ticket__check-title");
+      const expected1 = await string1;
+      expect(actual1).contains(expected1);
+      const seats = await getText(this.page, "body > main > section > div > p:nth-child(2) > span");
+      const actual2 = await seats.split(",")[0].split("/")[0];
+      const expected2 = await string4;
+      expect(actual2).equal(expected2);
+      const actual3 = await seats.split(",")[0].split("/")[1];
+      const expected3 = await string2;
+      expect(actual3).contains(expected3);
+      const actual4 = await seats.split(",")[1].split("/")[0].trim();
+      const expected4 = await string4;
+      expect(actual4).equal(expected4);
+      const actual5 = await seats.split(",")[1].split("/")[1];
+      const expected5 = await string3;
+      expect(actual5).contains(expected5);
+    });
+
+    When("user books a ticket for the session tomorrow at {string}, seat {string} in the {string} st row, and this seat is already reserved", 
+    { timeout: 15000}, async function (string1, string2, string3) {
+      await clickElement(this.page, "body > nav > a:nth-child(2)");
+      await reservationOneOrTwoTiketsXPath(this.page, `//*[text()='${string1}']`, 
+      `body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(${string3}) > span:nth-child(${string2})`, 
+      "body > main > section > div.buying__info > div > h2",
+      "//*[text()='Забронировать']");
+      await clickElement(this.page, ".acceptin-button");
+      await this.page.goto("http://qamid.tmweb.ru/client/index.php");
+      await clickElement(this.page, "body > nav > a:nth-child(2)");
+      await reservationOneOrTwoTiketsXPath(this.page, `//*[text()='${string1}']`, 
+      `body > main > section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(${string3}) > span:nth-child(${string2})`, 
+      "body > main > section > div.buying__info > div > h2",
+      "//*[text()='Забронировать']");
+    });
+
+    Then("user sees that the resrvation button is inactive", async function() {
+      const actual = await this.page.$eval("button.acceptin-button", (link) => link.getAttribute("disabled"));
+      await expect(actual).contain("true");
+    });
